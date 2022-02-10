51efb241eb10eecea4b0480179375f15bd9718de
linusmuema <linus.m.muema@gmail.com>
2022-02-10 12:55:50 +0300

    - added some methods in homepage

--- lib/screens/auth/auth.controller.dart
+++ lib/screens/auth/auth.controller.dart
@@ -1,3 +1,20 @@
+import 'package:crs/services/hive.service.dart';
+import 'package:crs/services/network.service.dart';
 import 'package:get/get.dart';
 
-class AuthController extends GetxController {}
+class AuthController extends GetxController {
+  final HiveService hiveService = Get.find();
+  final NetworkService networkService = Get.find();
+
+  RxInt page = 0.obs;
+
+  void changePage(int index) => page.value = index;
+
+  void login() async {
+    // TODO: implement the logic
+  }
+
+  void signup() async {
+    // TODO: implement the logic
+  }
+}
