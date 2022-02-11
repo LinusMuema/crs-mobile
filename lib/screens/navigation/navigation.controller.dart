a2ee831ea64cf70042e1f079768c41200f8183a3
linusmuema <linus.m.muema@gmail.com>
2022-02-11 16:11:25 +0300

    - added the bottom nav

--- lib/screens/navigation/navigation.controller.dart
+++ lib/screens/navigation/navigation.controller.dart
@@ -1,3 +1,8 @@
 import 'package:get/get.dart';
 
-class NavigationController extends GetxController {}
+class NavigationController extends GetxController {
+  RxInt selected = 0.obs;
+  List<String> screens = ['Discover', 'Transactions', 'Garage', 'Profile'];
+
+  void navigate(int index) => selected.value = index;
+}
--- lib/screens/navigation/navigation.dart
+++ lib/screens/navigation/navigation.dart
@@ -1,5 +1,8 @@
 import 'package:crs/screens/navigation/navigation.controller.dart';
+import 'package:crs/theme/colors.dart';
+import 'package:crs/theme/dimens.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class Navigation extends GetView<NavigationController> {
@@ -7,6 +10,35 @@ class Navigation extends GetView<NavigationController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Obx(() {
+      var index = controller.selected.value;
+      return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 0,
+          currentIndex: index,
+          selectedFontSize: 12,
+          showSelectedLabels: true,
+          selectedItemColor: black,
+          onTap: controller.navigate,
+          showUnselectedLabels: false,
+          type: BottomNavigationBarType.fixed,
+          items: controller.screens.map((e) {
+            var icon = e.toLowerCase();
+            var selected = index == controller.screens.indexOf(e);
+            return BottomNavigationBarItem(
+              label: e,
+              icon: Padding(
+                padding: smallInsets,
+                child: SvgPicture.asset(
+                  'assets/images/$icon.svg',
+                  width: Get.width * .05,
+                  color: selected ? black : grey,
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      );
+    });
   }
 }
