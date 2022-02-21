742b0ece3ad4208dd655b498ae5d449c2e578047
linusmuema <linus.m.muema@gmail.com>
2022-02-21 16:48:41 +0300

    - updated the details

--- lib/screens/details/details.controller.dart
+++ lib/screens/details/details.controller.dart
@@ -1,3 +1,18 @@
+import 'package:crs/models/vehicle.model.dart';
+import 'package:crs/services/hive.service.dart';
+import 'package:crs/services/network.service.dart';
+import 'package:crs/utils/constants.dart';
 import 'package:get/get.dart';
 
-class DetailsController extends GetxController {}
+class DetailsController extends GetxController {
+  final HiveService hiveService = Get.find();
+  final NetworkService networkService = Get.find();
+
+  Rxn<Vehicle> vehicle = Rxn();
+
+  @override
+  void onInit() {
+    vehicle.value = hiveService.get(Constants.VEHICLE);
+    super.onInit();
+  }
+}
--- lib/screens/details/details.dart
+++ lib/screens/details/details.dart
@@ -1,4 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:crs/components/loaders.dart';
 import 'package:crs/screens/details/details.controller.dart';
+import 'package:crs/theme/colors.dart';
+import 'package:crs/theme/dimens.dart';
+import 'package:crs/theme/typography.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -7,6 +13,103 @@ class Details extends GetView<DetailsController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Obx(() {
+      var vehicle = controller.vehicle.value!;
+      return Scaffold(
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      autoPlay: true,
+                      viewportFraction: 1,
+                      aspectRatio: Get.height * .0015,
+                    ),
+                    items: vehicle.images.map((e) {
+                      return CachedNetworkImage(
+                        imageUrl: e,
+                        width: Get.width,
+                        fit: BoxFit.cover,
+                        placeholder: (c, i) => threeBounce(color: black),
+                      );
+                    }).toList(),
+                  ),
+                  verticalSpaceTiny,
+                  Expanded(
+                    child: SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Padding(
+                        padding: regularHInsets,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(vehicle.getName()!, style: heading1),
+                                Text('Ksh ${vehicle.rate}/hr', style: heading4),
+                              ],
+                            ),
+                            verticalSpaceTiny,
+                            Row(
+                              children: [
+                                Text(vehicle.plate, style: body1),
+                                horizontalSpaceTiny,
+                                Text('•', style: body1),
+                                horizontalSpaceTiny,
+                                Text(vehicle.color, style: body1)
+                              ],
+                            ),
+                            verticalSpaceSmall,
+                            Row(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: fullRadius,
+                                  child: CachedNetworkImage(
+                                    fit: BoxFit.cover,
+                                    width: Get.width * .15,
+                                    imageUrl: vehicle.user.avatar,
+                                    placeholder: (c, i) => pulse(color: black),
+                                  ),
+                                ),
+                                horizontalSpaceSmall,
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(vehicle.user.username, style: body1),
+                                    Text(vehicle.user.phone, style: body1)
+                                  ],
+                                ),
+                              ],
+                            ),
+                            verticalSpaceSmall,
+                            Text(vehicle.description, style: body1),
+                            verticalSpaceLarge,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: smallInsets,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Request', style: body2),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    });
   }
 }
